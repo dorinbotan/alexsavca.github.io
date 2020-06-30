099489abe59f8170c8879967e977f28c71e03ab3
AlexSavca <63048347+AlexSavca@users.noreply.github.com>
2020-06-30 14:29:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let i = prompt("Salut, esti prima data aici?");
+
+  switch (i) {
+    case 'da': alert('Bine ai venit!')
+    break;
+    case 'Da': alert('Bine ai venit!')
+    break;
+    case 'nu': alert('Salut')
+    break;
+    case 'Nu': alert('Salut')
+    break;
+    default: alert('Raspuns incorect')
+
+  }
